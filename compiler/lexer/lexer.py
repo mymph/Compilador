3e90813b82faaf03e1f963ad8fbde6ce0c82d506
mymph <leticialivia7991@gmail.com>
2024-07-13 22:53:26 -0300

    Tokens da linguagem

--- compiler/lexer/lexer.py
+++ compiler/lexer/lexer.py
@@ -1,1 +1,6 @@
-
+'''O Analisador léxico pega os caracteres de entrada e os trasnforma em tokens
+- Lê os caracteres de entrada e os agrupa em lexemas
+- Ignora espaços, tabulações e saltos de linha.
+Ex.: o número 25432 de entrada será um token, em vez de cada número ser um token
+- Token é um terminal. Possui atributos com informações adicionais sobre o símbolo.
+Ex.: valor, tipo, escopo, etc.'''
